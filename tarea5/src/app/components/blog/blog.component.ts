77d0ad15952c9f003eb224286056944a2f503bf3
Gabriel_Robles <gabrielroblescuev@gmail.com>
2025-02-22 19:49:14 +0100

    Criterio 5

--- tarea5/src/app/components/blog/blog.component.ts
+++ tarea5/src/app/components/blog/blog.component.ts
@@ -8,8 +8,9 @@ import { INotices } from '../../interfaces/i-notices';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-  @Input() listacalculos: INotices[] = [
-    {title: "Prueba", image:"https://placehold.co/300x200", text:"texto de prueba", date: "a"},
-    {title: "Prueba2", image:"https://placehold.co/300x200", text:"texto de prueba 2", date: "b"}
+  @Input() listaNoticias: INotices[] = [
+    {title: "Prueba", image:"", text:"texto de prueba", date: "a"},
+    {title: "Prueba2", image:"https://placehold.co/300x200", text:"texto de prueba 2", date: "b"},
+    {title: "Prueba", image:"https://placehold.co/300x200", text:"texto de prueba 3", date: "c"}
   ]
 }
